e2935a8e94ffccb2b0529f69e74809b661c04cec
Coluzeus09 <146698023+Colozeus005@users.noreply.github.com>
2023-10-29 18:03:41 +0800

    Add CSS Properties

--- line_spacing/script.js
+++ line_spacing/script.js
@@ -26,3 +26,13 @@ function change() {
 
 input.addEventListener('input', change);
 input.addEventListener('change', change);
+
+function setTheme() {
+  const root = document.documentElement;
+  const newTheme = root.className === 'dark' ? 'light' : 'dark';
+  root.className = newTheme;
+  
+  document.querySelector('.theme-name').textContent = newTheme;
+}
+
+document.querySelector('.theme-toggle').addEventListener('click', setTheme)
